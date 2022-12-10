0a18bc62b70b0575e7fbbf04047d5a568826d3b1
Kyrylo Silin <silin@kyrylo.org>
2022-12-11 00:46:41 +0200

    01-calorie-counting: solve part 2

--- 01-calorie-counting/src/main.rs
+++ 01-calorie-counting/src/main.rs
@@ -59,12 +59,35 @@
 //
 // Find the Elf carrying the most Calories. How many total Calories is that Elf
 // carrying?
+//
+// --- Part Two ---
+//
+// By the time you calculate the answer to the Elves' question, they've already
+// realized that the Elf carrying the most Calories of food might eventually run
+// out of snacks.
+//
+// To avoid this unacceptable situation, the Elves would instead like to know
+// the total Calories carried by the top three Elves carrying the most Calories.
+// That way, even if one of those Elves runs out of snacks, they still have two
+// backups.
+//
+// In the example above, the top three Elves are the fourth Elf (with 24000
+// Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with
+// 10000 Calories). The sum of the Calories carried by these three elves is
+// 45000.
+//
+// Find the top three Elves carrying the most Calories. How many Calories are
+// those Elves carrying in total?
 
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() {
+    part2(part1());
+}
+
+fn part1() -> Vec<i32> {
     let mut totals: Vec<i32> = vec![];
     let f = File::open("src/input.txt").expect("Expected to read src/input.txt");
     let buf = BufReader::new(f);
@@ -87,7 +110,17 @@ fn main() {
     }
 
     println!(
-        "Total calories: {}\nhttps://adventofcode.com/2022/day/1",
+        "https://adventofcode.com/2022/day/1\nTotal calories: {}",
         totals.iter().max().unwrap()
+    );
+
+    totals
+}
+
+fn part2(mut totals: Vec<i32>) {
+    totals.sort();
+    println!(
+        "Top 3 total: {:?}",
+        totals.iter().rev().take(3).sum::<i32>()
     );
 }
